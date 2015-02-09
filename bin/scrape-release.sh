244f31b258c5f7099203b6e4fcffc55cdd190f40
Brian Porter <beporter@users.sourceforge.net>
2015-02-09 13:09:12 -0600

    scrape-release.sh produces a local release/ folder.
    Next steps are to get that folder committed to the repo automatically.
    
    Need docs too.

--- bin/scrape-release.sh
+++ bin/scrape-release.sh
@@ -3,7 +3,7 @@
 
 DIR="$( cd -P "$( dirname "$0" )"/.. >/dev/null 2>&1 && pwd )"
 
-SOURCE_URL=http://puphpet.com
+SOURCE_URL="https://puphpet.com"
 DEST_DIR="${DIR}/tmp"
 DEST_FILE="puphpet.zip"
 DEST_PATH="${DEST_DIR}/${DEST_FILE}"
@@ -20,12 +20,6 @@ for EXEC in "${EXECUTABLES[@]}"; do
 done
 
 
-# Scrape the form we need to submit from the source site.
-curl -L $SOURCE_URL | "${DIR}/bin/formfind.pl" > "${DEST_DIR}/form.txt"
-cat "${DEST_DIR}/form.txt"
-exit
-
-
 # Scrape the ZIP from the source site.
 echo "## Starting ZIP file scrape."
 
@@ -35,14 +29,16 @@ if [ ! -d "${DEST_DIR}" ]; then
 fi
 
 echo "## Submitted request to \`${DEST_DIR}\`."
-curl -L \
- --output "${DEST_PATH}" \
- --data ''\
- $SOURCE_URL
+# curl -L \ # @TODO: Re-enable.
+#  --silent \
+#  --output "${DEST_PATH}" \
+#  --dump-header "${DIR}/tmp/request_headers.txt" \
+#  --data 'vagrantfile-local[vm][box]=puphpet/debian75-x64' \ # We only need to POST a non-empty <form> to get the ZIP.
+#  $SOURCE_URL
 
 echo "## Done."
 
-#exit 0
+#exit 0  #@TODO: Testing
 
 
 
@@ -50,9 +46,46 @@ echo "## Done."
 
 # Unpack the zip to our target folder, replacing the existing one if present.
 SOURCE_ZIP="${DIR}/tmp/puphpet.zip"
+TMP_UNZIP_DIR="${DIR}/tmp/unzip"
+RELEASE_DIR="${DIR}/release"
 
+
+if [ -f "${SOURCE_ZIP}" ]; then
+	echo "## Removing existing destination dir \`${TMP_UNZIP_DIR}\`."
+	rm -rf "${TMP_UNZIP_DIR}"
+fi
+
+echo "## Extracting temp archive \`${SOURCE_ZIP}\` to \`${TMP_UNZIP_DIR}\`."
 unzip \
+ -oqq \
  "${SOURCE_ZIP}" \
- -d "${DIR}/unzip"
+ -d "${TMP_UNZIP_DIR}"
+
+# Get the random folder name inside the extracted folder.
+RANDOM_SUB_DIR=$(cd -P "${TMP_UNZIP_DIR}" >/dev/null 2>&1; echo *)
+
+mv -f "${TMP_UNZIP_DIR}/${RANDOM_SUB_DIR}" "${RELEASE_DIR}"
+
+
+
+#exit 0   #@TODO: Testing
+
+
+
+
+# Commit any and all changes to the release/ folder back into git.
+# Use available local information to tag the commit for future identification.
+
+GIT_COMMIT_MSG="Auto-release. `date`."
+
+git add -A "${RELEASE_DIR}"
+
+#git commit -m "${GIT_COMMIT_MSG}"
+
+
+
+
+
+
 
 
